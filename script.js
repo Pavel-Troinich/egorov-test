f367ce5aa28efc864db97f2a54e7c553278bfafe
Pavel <tr.pav.ar@gmail.com>
2023-06-11 17:02:47 +0300

    feat: implement subscribe

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const END_TIME = '2023-07-24';
+const EMAIL_REGEXP = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const days = document.querySelector('.timer__days');
 const hours = document.querySelector('.timer__hours');
 const minutes = document.querySelector('.timer__minutes');
 const seconds = document.querySelector('.timer__seconds');
+const subscriptionInput = document.querySelector('.subscription__input');
+const subscriptionBtn = document.querySelector('.subscription__btn');
+
+const modal = document.querySelector('.modal');
+const modalTitle = document.querySelector('.modal__title');
+const modalNotice = document.querySelector('.modal__notice');
+const modalCloseIcon = document.querySelector('.modal__close');
+const modalCloseBtn = document.querySelector('.modal__btn');
+
+subscriptionBtn.addEventListener('click', sendEmail);
+modal.addEventListener('click', closeModal);
+modalCloseIcon.addEventListener('click', closeModal);
+modalCloseBtn.addEventListener('click', closeModal);
+
 
 function getRemainingTime(endTime){
   const remainingTime = Date.parse(endTime) - Date.parse(new Date());
@@ -35,6 +50,38 @@ function setTimer() {
       clearInterval(timer);
     }
   }, 1000);
+}
+
+function subscribe() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => {
+    if (res.status === 200) {
+      modalTitle.innerHTML = 'Success!';
+      modalNotice.innerHTML = 'You have successfully subscribed to the email newsletter';
+    } else {
+      modalTitle.innerHTML = 'Error!';
+      modalNotice.innerHTML = 'An error has occurred. Try again later.';
+    }
+  })
+  .catch(err => console.log(err));
+}
+
+function closeModal() {
+  modal.classList.remove('active');
+}
+
+function validateEmail (email) {
+  return email.match(EMAIL_REGEXP);
+};
+
+function sendEmail() {
+  if (validateEmail(subscriptionInput.value)) {
+    subscribe();
+  } else{
+    modalTitle.innerHTML = 'Wrong email!';
+    modalNotice.innerHTML = 'Check the email and try again.';
+  }
+  modal.classList.add('active');
 }
 
 setTimer();
